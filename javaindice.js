3e6a04c83b41ceb2e69d2b3215f6f344cda94476
Humberto <humberto.aranguiz@gmail.com>
2023-04-18 14:01:30 -0400

    merge nueva rama

---
+++ javaindice.js
@@ -0,0 +1,13 @@
+const fn = () =>{
+    const miString = "esto es un string dybbuk"
+    let b = miString + " se estrenará ep + página"
+    console.log(b)
+}
+
+fn()
+
+const clikeando = () => {
+    let a = "y luego disco"
+
+    console.log(a)
+}
